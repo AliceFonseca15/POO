56a9cc3a9bdbc74a407c9db11d815539a85be3ee
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-04-14 17:50:24 -0300

    Create ex2.py

---
+++ Aula_04/ex2.py
@@ -0,0 +1,25 @@
+#Aula de introdução aos conceitos de POO
+# Entidades, classificação, modelos de entidade e abstra~ção
+
+class triangulo:
+        def __init__(self):
+             self.b = 0
+             self.h = 0
+        def teste(self):
+            return "Olá"
+        def calc_area(self): # self é usado para o pyhton saber qual a variavel é usada na operação
+              return self.b * self.h / 2
+x = triangulo()  # executa o método __init__
+print(x)
+print(x.b,x.h)
+x.b = 10
+x.h = 20
+print(x.teste())
+print(x.b,x.h)
+print(x.calc_area())
+
+y = triangulo()
+print(y)
+print(y.teste)
+print(y.b,y.h)
+print(y.calc_area())
