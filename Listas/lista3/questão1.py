e2706ab63540fbbd222bde17b5778a67190a08e7
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-04-30 17:24:02 +0000

    lista3 - inacabada

---
+++ Listas/lista3/questão1.py
@@ -0,0 +1,17 @@
+class retangulo():
+    def __init__(self,b,a):
+       if a <=0:
+           self.__a = a
+       else:
+            raise ValueError()
+       if b <=0:
+           self.__b = b
+       else:
+            raise ValueError()
+        
+    def calc_retangulo(self):
+        return self.__b * self.__a
+    
+    def calc_diagonal(self):
+        return 
+      
