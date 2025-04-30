03181fbb1eeeee038c1969789eea29d40296377d
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-04-30 17:26:03 +0000

    não finalizada

--- Listas/lista3/questão1.py
+++ Listas/lista3/questão1.py
@@ -8,7 +8,12 @@ class retangulo():
            self.__b = b
        else:
             raise ValueError()
-        
+    def set_base(self):
+
+    
+    def set_altura(self):
+
+
     def calc_retangulo(self):
         return self.__b * self.__a
     
